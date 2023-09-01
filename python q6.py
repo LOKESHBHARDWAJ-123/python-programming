4cb7af02f39d574c62b3079b857e5358d39f4f3f
LOKESHBHARDWAJ-123 <143807924+LOKESHBHARDWAJ-123@users.noreply.github.com>
2023-09-01 05:19:07 -0700

    days in to weeks

---
+++ python q6.py
@@ -0,0 +1,14 @@
+# question no6
+a=int(input("enter the first no"))
+b=int(input("enter the second no"))
+c=a**b
+print(c)
+
+
+# question no7
+l=int(input("enter the length"))
+b=int(input("enter the breadth"))
+a=l*b
+p=2*(l+b)
+print("Area is ",a)
+print("Perimeter is",p)
